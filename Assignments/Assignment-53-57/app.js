5d39ad554f0c63dad4fc815fbf5bf20542cb92b9
LaibaKhan-dev <125359983+LaibaKhan-dev@users.noreply.github.com>
2023-06-10 00:24:34 +0500

    Update app.js

--- Assignments/Assignment-53-57/app.js
+++ Assignments/Assignment-53-57/app.js
@@ -5,6 +5,24 @@
 
 
 // Q.1
+// function openModal(img) {
+//   var modal = document.getElementById('myModal');
+//   var modalImg = document.getElementById('img');
+//   modal.style.display = 'block';
+//   modalImg.src = img.src;
+  
+//   var span = document.getElementsByClassName('close')[0];
+//   span.onclick = function(){
+//     modal.style.display = 'none';
+//   }
+// }
+
+
+
+
+
+
+// Q.2
 
   // function zoomIn() {
   //   var pic = document.getElementById("pic");
